4375ec893dd50fb664e409d62839239da3ea705b
alsantos10 <alsantos1@gmail.com>
2026-09-04 00:32:49 -0300

    Ajustes configs

--- apps/marketing/src/app/layout.tsx
+++ apps/marketing/src/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
+      lang="pt-BR"
       className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
