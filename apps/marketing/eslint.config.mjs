46c998dcf64e6f736d457ece374f423f959c4004
alsantos10 <alsantos1@gmail.com>
2026-09-09 00:10:36 -0300

    feat: add docker ci strategy scaffold

--- apps/marketing/eslint.config.mjs
+++ apps/marketing/eslint.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-plugin-next-vitals";
 import nextTs from "eslint-config-next/typescript";
 
 const eslintConfig = defineConfig([
-    ...nextVitals,
     ...nextTs,
     globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
 ]);
